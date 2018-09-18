4a14af1110b0ef7406044d59381f0f242fe93c9a
Asa6 <[18518353480@163.com]>
2018-09-18 19:25:07 +0800

    可折叠侧边栏优化

--- static/js/sidebar_folding.js
+++ static/js/sidebar_folding.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
     $(".toggle-sidebar").click(function () {
         $("#sidebar").toggleClass("collapsed");
         $("#content").toggleClass("col-md-12 col-md-10 col-md-offset-0");
-
         return false;
     });
 });
