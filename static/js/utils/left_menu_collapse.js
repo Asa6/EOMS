0122e51d0db1672410d0a1b66f2017c616eb422d
HaoYuan.Geng <18518353480@163.com>
2018-09-22 15:45:02 +0800

    侧边栏二级菜单折叠优化

---
+++ static/js/utils/left_menu_collapse.js
@@ -0,0 +1,18 @@
+$('#collapse1').on('show.bs.collapse', function () {
+    $("#setup-menu-icon").removeClass("glyphicon-chevron-left");
+    $("#setup-menu-icon").addClass("glyphicon-chevron-down");
+});
+
+
+$('#collapse1').on('hide.bs.collapse', function () {
+    $("#setup-menu-icon").removeClass("glyphicon-chevron-down");
+    $("#setup-menu-icon").addClass("glyphicon-chevron-left");
+});
+
+
+$(function () {
+    if (window.location.pathname === "/cmdb/user_manager") {
+        $("#collapse1").addClass("in");
+        $("#setup-menu-icon").toggleClass("glyphicon-chevron-down");
+    }
+});
