703c6b552366c08358d71f6b137ec3bb6123d007
HaoYuan.Geng <18518353480@163.com>
2018-09-28 09:19:17 +0800

    DashBoard:     持续迭代前端界面

--- static/js/utils/sidebar_folding.js
+++ static/js/utils/sidebar_folding.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     $(".toggle-sidebar").click(function () {
         $("#sidebar").toggleClass("collapsed");
         $("#content").toggleClass("col-md-12 col-md-10 col-md-offset-0");
+
         return false;
     });
 });
